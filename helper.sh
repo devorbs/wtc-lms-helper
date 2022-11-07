380d532b32f4d6a202acb5356a54b466c0977bdd
devorbs <98650142+devorbs@users.noreply.github.com>
2022-11-07 18:56:42 +0200

    Update helper.sh
    added comments

--- helper.sh
+++ helper.sh
@@ -6,28 +6,39 @@ read project
 echo -n "What iteration? (Enter '1' for iteration 1.. etc): "
 read iteration_number
 
+#Set the iteration number and concat to path, get project name
 iteration='iteration'$iteration_number
 project_uiid=$1
 
+#check if project name is not found from cli arguments
 if [ -z "$project_uiid" ]; 
 then
+	#Prompt user for the project name
 	echo "Enter the project uiid"
 	read project_uiid
 fi
-	DIR=~/problems/$project/$iteration/$project_name
-	wtc-lms start $project_uiid > gitlab_link.txt;
-	link=$(grep "git" "gitlab_link.txt");
-	if [ -d "$DIR" ]; 
-	then 
-		cd $DIR;
-		echo "Directory already exists.";
-		code $DIR;
-		$SHELL;
-	else
-		$link $DIR;
-		cd $DIR;
-		code $DIR;
-		$SHELL
-	fi
+#set a directory path to save the clone into
+DIR=~/problems/$project/$iteration/$project_name
+
+#command lms to start a project with project name, save to file
+#get the file and search for git clone command
+wtc-lms start $project_uiid > gitlab_link.txt;
+link=$(grep "git" "gitlab_link.txt");
+
+#check if the directory to clone into exists
+if [ -d "$DIR" ]; 
+then 
+	# move to that directory and open it with a code editor
+	cd $DIR;
+	echo "Directory already exists.";
+	code $DIR;
+	$SHELL; #opening child terminal
+else
+	#clone from github into the directory then open with code editor
+	$link $DIR;
+	cd $DIR;
+	code $DIR;
+	$SHELL #opening child terminal
+fi
 
 
